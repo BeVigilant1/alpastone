b5b07877aeedee0fbd36cea34db5fce0a78ea8ac
Danya <disproziy@gmail.com>
2017-06-21 04:16:45 +0300

    (feat) Content upload

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,12 @@
+jQuery(document).ready(function($) {
+$(window).load(function(){
+  $('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+});
+
+
+
 $(function() {
     var navbar = $(".navbar");
     $(window).scroll(function() {
@@ -9,3 +18,22 @@ $(function() {
         }
     });
 });
+
+
+// function interpolation() {
+//     const data = [
+//         'http://res.cloudinary.com/drmhxontt/image/upload/v1498005699/Alpa Stone/decor_kamin3.jpg',
+//         '../img/decor_lestnica.jpg',
+//          '../img/decor_kuhnya.jpg',
+//          '../img/decor_oblicovka_sten.jpg'
+//     ];
+//     const item = 0;
+//     for (item < 3) {
+//     elem = document.getElementById("decor");
+//     elem.style.backgroundImage = "url("+data[item]+")";
+//     item++;
+//     }
+// };
+
+
+// interpolation();
